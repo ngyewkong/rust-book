715e08bfa7dbdc71606f36ba562418f45ae9c8f1
Ng Yew Kong <ngyewkong1996@gmail.com>
2024-06-01 22:32:32 +0800

    initial commit

---
+++ hello_world/main.rs
@@ -0,0 +1,8 @@
+// main() function is the first code that runs in every executable rust program
+// to execute a rust program
+// need to compile the program using rustc main.rs and create the executable main file
+// ./main to run the executable file
+fn main() {
+    // println! is a macro (not a function as it has the ! at the back)
+    println!("Hello, world!");
+}
